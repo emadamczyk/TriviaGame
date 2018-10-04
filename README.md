919097ce242898f591c0c3b59fc5a81ad729fb3f
Elizabeth Adamczyk <emadamczyk@hotmail.com>
2018-10-04 00:53:22 -0500

    got it working + updated readme file

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # TriviaGame
+
+This game is designed using javascrip and jQuery to use dynamic htmal and a countdown timer in the form of a trivia game. The player has a fixed limited amount of time to complete the quiz. If the player does not complete the game in time, the game alerts "Time's Up!" and then displays the number of questions answered correctly, incorrectly, and unanswered. If the player answers all questions before time runs out, the player clicks the "Done" button and then the next page displays the number of questions answered correctly, incorrectly, and unanswered. Players are not allowed to select more than one answer per question. 
+
+Users click the "Start" button to start the countdown timer and begin answering questions. 
+
+In the more advanced version of this game, the game would only display one question per page with a countdown timer for each question. It also alerts the player if the question was answered correctly or incorrectly before advancing to the next question. A game summary of questions answered correctly, incorrectly, or left unanswered is displayed. They player then gets an option to play again.
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,27 +11,27 @@ $(document).on("click", "#done", function() {
 
 var questions = [
     {
-        question: "Bicycling is the official sport of the state of Illinois.",
+        question: "1) Bicycling is the official sport of the state of Illinois.",
         answers: ["True", "False"], 
         correctAnswer: "True"
     },
     {
-        question: "Chicago's Divvy bike share program is the largest bike share program in the United States.",
+        question: "2) Chicago's Divvy bike share program is the largest bike share program in the United States.",
         answers: ["True", "False"],
         correctAnswer: "False"
     },
     {
-        question: "In the late 1800s Chicago was home to more than 70 bicycle manufacturers.",
+        question: "3) In the late 1800s Chicago was home to more than 70 bicycle manufacturers.",
         answers: ["True", "False"],
         correctAnswer: "True"
     },
     {
-        question: "Pedicabs are banned from riding on Michigan Avenue and State Street in downtown Chicago.",
+        question: "4) Pedicabs are banned from riding on Michigan Avenue and State Street in downtown Chicago.",
         answers: ["True", "False"],
         correctAnswer: "True"
     },
     {
-        question: "Cyclists have the same rights as motorists and must follow the rules applicable to a motorist.",
+        question: "5) Cyclists have the same rights as motorists and must follow the rules applicable to a motorist.",
         answers: ["True", "False"],
         correctAnswer: "True"
     }];
@@ -39,28 +39,33 @@ var questions = [
 var game = {
     correct: 0,
     incorrect: 0,
-    counter: 60, 
+    counter: 10, 
     countdown: function() {
         game.counter--;
         $("#counter").html(game.counter);
         if (game.counter <= 0) {
             console.log("Time's Up!");
-            game.done();
+            $("#game").prepend("<h3>Time's Up!</h3>");
+            delayedAlert = setTimeout(function() {
+                game.done();
+              }, 1000);
+            
         }
     },
     start: function() {
         timer = setInterval(game.countdown, 1000);
-        console.log(counter)
-        //$("#game").prepend("<h3>Time Remaining: <span id="counter"> 60 </span> seconds</h3>");
+        console.log(game.counter)
+        $("#game").prepend("<h3>Time Remaining: <span id='counter'> 60 </span> seconds</h3>");
         $("#start").remove();
+        $("#game").empty();
         for (var i = 0; i < questions.length; i++) {
-            $("#game").append("<p>" + questions[i].question + "</p>");
+            $("#game").append("<br><br><p>" + questions[i].question + "</p>");
             for (var j = 0; j < questions[i].answers.length; j++) {
-                $("#game").append("<input type='radio' name='question-'" + i + "' value=' " +  questions[i].answers[j] +"' >" + questions[i].answers[j])
+                $("#game").append(" " + "<input type='radio' name='question-'" + i + "' value=' " +  questions[i].answers[j] +"' >" + " " + questions[i].answers[j]);
             }
         }
     
-    $("#game").append($("<button id='done'>DONE</button>"));
+    $("#game").append($("<br><br><button id='done'>DONE</button>"));
     },
     done: function() {
         $.each($("input[name='question-0']:checked"), function() {
